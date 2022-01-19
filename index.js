3349d1aaec87320fb1d033b25ab8fa151fd913f1
Melissa Vela <melissa@instarent.com.au>
2022-01-19 11:58:42 +1100

    connection

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+var mysql = require('mysql2');  
+var con = mysql.createConnection({  
+  host: "localhost",  
+  user: "root",  
+  password: "Root@123"  
+});  
+con.connect(function(err) {  
+  if (err) throw err;  
+  console.log("Connected!");  
+});  
+
+
+
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res)=> {
+app.get('/task', (req, res)=> {
     res.send('haha')
+
+    const sqlSelect = "SELECT * FROM task"
+    con.query(sqlSelect, (err, result)=> {
+        console.log(result)
+    })
 
 });
 
@@ -14,3 +32,5 @@ app.listen(app.get('port'), ()=>{
     console.log('server on port', app.get('port'));
  
  });
+
+
